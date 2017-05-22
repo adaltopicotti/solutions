789595ae68490259cdec3b90f13ea9408ecd4096
Adalto Picotti Junior <picotti.junior@gmail.com>
2017-05-22 14:34:39 -0300

    Update views.py

--- cotador/views.py
+++ cotador/views.py
@@ -7,22 +7,24 @@ def quotation(request):
         price = request.POST['price']
         area = request.POST['area']
         city = request.POST['city']
-        result = calc(float(price), float(area))
+        result = calc(float(price), float(area), city)
         city_sel = City.objects.get(id=city)
         return render(request, 'cotador/cotador.html', {'cities': cities,
                                                     'total_cost': repr(result[0]),
                                                     'final_cost': repr(result[1]),
                                                     'subv_fed': repr(result[2]),
                                                     'is_total': repr(result[3]),
-                                                    'city': city_sel})
+                                                    'city': city_sel,
+                                                    'prod_esp': result[4]})
 
 
     return render(request, 'cotador/cotador.html', {'cities': cities})
 
 
-def calc(price, area):
+def calc(price, area, city):
+    prod_esp = Prod_Esp.objects.get(id=city)
     is_total = round(((price * 24.33) *area),2)
     total_cost = round(is_total * 0.165,2)
     subv_fed = round(total_cost * 0.45,2)
     final_cost = round(total_cost - subv_fed,2)
-    return [total_cost, final_cost, subv_fed, is_total]
+    return [total_cost, final_cost, subv_fed, is_total, prod_esp]
