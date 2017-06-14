f4fdff0eaeeadb9998506d9811628d099ca3b008
Adalto Picotti Junior <picotti.junior@gmail.com>
2017-06-14 15:24:40 -0300

    Create views.py

--- cotador/views.py
+++ cotador/views.py
@@ -25,6 +25,7 @@ def quotation(request):
             insured_error = ""
             return render(request, 'cotador/multirrisco.html', {
                 'pages': pages,
+                'cpf_cnpj': cpf_cnpj,
                 'insured_name': insured.nome
                 })
         except:
