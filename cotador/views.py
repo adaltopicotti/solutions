fe1fecf01193abbe2b6f9c649fbc07c01744a397
Adalto Picotti Junior <picotti.junior@gmail.com>
2017-06-19 15:19:01 -0300

    Update views.py

--- cotador/views.py
+++ cotador/views.py
@@ -8,7 +8,7 @@ def cpfcnpj_request(cpf_cnpj):
     if validate_cpf(cpf_cnpj):
         try:
             insured = Insured.objects.get(cpf_cnpj=cpf_cnpj)
-            if insured.name
+            if insured.name:
                 return insured.name
             else:
                 return "now"
