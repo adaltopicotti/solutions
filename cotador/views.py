9b4565a0c472c228790ec4fdf5d179b99a67e107
Adalto Picotti Junior <picotti.junior@gmail.com>
2017-09-15 16:40:20 -0300

    Update views.py

--- cotador/views.py
+++ cotador/views.py
@@ -105,7 +105,7 @@ def quotation(request):
             'products': products,
             'cpf_cnpj': cpf_cnpj,
             'insured_name': insured,
-            'ufs': ufs
+            'ufs': ufs,
             'ind': ind
             })
 
