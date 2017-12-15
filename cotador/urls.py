d7e7a80400fdcb1cdc61d9fec5a6ea976e458a38
Adalto Picotti Junior <picotti.junior@gmail.com>
2017-12-15 10:37:06 -0200

    Update urls.py

--- cotador/urls.py
+++ cotador/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^pdc/$', views.pdc, name='pdc'),
     url(r'^pdc/add/$', views.add, name='add'),
     url(r'^pdc/weather/$', views.weather, name='weather'),
-    url(r'^pdc/View/$', views.weather_data, name='weather_data'),
+    url(r'^pdc/view/$', views.weather_data, name='weather_data'),
     url(r'^quotation/new/$', views.quotation_new, name='quotation_new'),
 ]
